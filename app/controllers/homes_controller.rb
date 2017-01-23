c0eedb26c6d030117fa4adc26555f60bbbcb4a3a
Trish Griego <pmgriego@gmail.com>
2017-01-23 11:44:50 -0800

    added notes about time

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
     @visits = Visit.all
   end
 
+#%B: full month name "January", #%d: Day of Month 01-31, #%Y:2007 
   def visits_by_day
      render json: Visit.group_by_day(:visited_at, format: "%B %d, %Y").count
   end

6f94877a2f6851188351e9889cf81a968c810e2b
Trish Griego <pmgriego@gmail.com>
2017-01-22 23:03:56 -0800

    Seeded database and created controller homes with show method.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Visit.delete_all
+Visit.create  country: 'United States', visited_at: DateTime.now, load_time: 3.5
+Visit.create  country: 'United States', visited_at: DateTime.now, load_time: 1.5
+Visit.create  country: 'United States', visited_at: DateTime.now, load_time: 1.0
+Visit.create  country: 'United States', visited_at: DateTime.now - 1.day, load_time: 4.5
+Visit.create  country: 'United States', visited_at: DateTime.now - 1.day, load_time: 4.0
+Visit.create  country: 'United States', visited_at: DateTime.now - 2.days, load_time: 3.5
+Visit.create  country: 'United States', visited_at: DateTime.now - 2.days, load_time: 1.0
+Visit.create  country: 'United States', visited_at: DateTime.now - 2.days, load_time: 3.5
+Visit.create  country: 'United States', visited_at: DateTime.now - 3.days, load_time: 4.5
+Visit.create  country: 'United States', visited_at: DateTime.now - 3.days, load_time: 3.0
+Visit.create  country: 'Germany', visited_at: DateTime.now, load_time: 1.0
+Visit.create  country: 'Germany', visited_at: DateTime.now, load_time: 2.0
+Visit.create  country: 'Germany', visited_at: DateTime.now, load_time: 1.0
+Visit.create  country: 'Germany', visited_at: DateTime.now, load_time: 3.0
+Visit.create  country: 'Germany', visited_at: DateTime.now - 1.day, load_time: 4.0
+Visit.create  country: 'Germany', visited_at: DateTime.now - 2.days, load_time: 2.0
+Visit.create  country: 'Germany', visited_at: DateTime.now - 2.days, load_time: 1.0
+Visit.create  country: 'Germany', visited_at: DateTime.now - 2.days, load_time: 3.0
+Visit.create  country: 'Germany', visited_at: DateTime.now - 3.days, load_time: 3.5
+Visit.create  country: 'South Korea', visited_at: DateTime.now, load_time: 2.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now, load_time: 2.5
+Visit.create  country: 'South Korea', visited_at: DateTime.now, load_time: 1.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now, load_time: 1.5
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 1.day, load_time: 2.5
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 1.day, load_time: 4.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 1.day, load_time: 3.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 2.days, load_time: 1.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 3.days, load_time: 5.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 3.days, load_time: 4.0
+Visit.create  country: 'South Korea', visited_at: DateTime.now - 3.days, load_time: 5.0

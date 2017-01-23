4d8a4acc0d1661a10d6391994e8da9397c6a7275
Trish Griego <pmgriego@gmail.com>
2017-01-22 23:45:47 -0800

    added several chart types. Unable to get ajax portion working. Changed config/initlizer file. Able to make global changes to color and style of charts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  # visits_by_day_home
+  #  GET '/home/visits_by_day' => 'homes#visits_by_day'
 
 end
